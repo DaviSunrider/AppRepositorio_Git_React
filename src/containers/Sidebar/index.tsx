89160023689ae124e8d389364d34b038f681edd9
DaviSunrider <115567218+DaviSunrider@users.noreply.github.com>
2024-02-07 22:34:51 -0300

    add componentes

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => {
+  ;<aside>
+    <img src="https://github.com/DaviSunrider" />
+    <Titulo>Davi Sunrider</Titulo>
+  </aside>
+}
+
+export default Sidebar
